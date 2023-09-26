0d22580ddee931e381319c1e5642ad3f3fc406b0
anthony baquerizo <anthonybaquerizo@gmail.com>
2023-09-26 16:47:12 -0500

    Vista general para mostrar el menu y la empresa

--- app/Common.php
+++ app/Common.php
@@ -13,3 +13,39 @@
  *
  * @see: https://codeigniter.com/user_guide/extending/common.html
  */
+
+ if (! function_exists('viewApp')) {
+    /**
+     * Metodo para cargar la vista junto con el menu, la empresa y tipo de cambio
+     */
+    function viewApp(string $page, string $name, array $data = [], array $options = []): string
+    {
+        $db = db_connect();
+        // menu
+        $fecha = date('Y-m-d');
+        $codEmpresa = $_COOKIE['empresa'];
+        $builder = $db->table('sidebar');
+        $sidebars   = $builder->get()->getResult('array');
+        // sub menu
+        $builder = $db->table('sidebardetalles');
+        $sidebardetalles   = $builder->get()->getResult('array');
+        // empresa
+        $builder = $db->table('empresas');
+        $empresa   = $builder->where('codEmpresa', $codEmpresa)->get()->getRow();
+        // tipo de cambio
+        $builder = $db->table('tipocambio');
+        $tipoCambio   = $builder->where('CodEmpresa', $codEmpresa)
+                    ->where('FechaTipoCambio', $fecha . ' 00:00:00')->get()->getRow();
+        
+        $data = array_merge($data, [
+            'page' => $page,
+            'sidebars' => $sidebars,
+            'sidebardetalles' => $sidebardetalles,
+            'razon_social' => $empresa->RazonSocial,
+            'ruc' => $empresa->Ruc,
+            'fecha' => date('d/m/Y'),
+            'tipo_cambio' => $tipoCambio,
+        ]);
+        return view($name, $data, $options);
+    }
+}
--- app/Views/app/layout/left_menu.php
+++ app/Views/app/layout/left_menu.php
@@ -120,8 +120,8 @@
                         </div>
                         <div class="fs-2 mb-6 text-dark w-100">
                             <div class="d-flex justify-content-between">
-                                <span class="fw-normal">Compra: <?= $tipo_cambio->compra ?></span>
-                                <span class="fw-normal">Venta: <?= $tipo_cambio->venta ?></span>
+                                <span class="fw-normal">Compra: <?= (isset($tipo_cambio)) ?? $tipo_cambio->compra ?></span>
+                                <span class="fw-normal">Venta: <?= (isset($tipo_cambio)) ?? $tipo_cambio->venta ?></span>
                             </div>
                         </div>
                     </div>
